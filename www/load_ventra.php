23768726b79a381f4c0058fb5280f1caa2e8d2bc
Reaplay <terran88@gmail.com>
2016-07-10 23:17:17 +0400

    Добавлены шаблоны страниц загрузки из  Ventra и SAP

---
+++ www/load_ventra.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: Reaplay
+     * Date: 10.07.2016
+     * Time: 23:15
+     */
+    require_once ("include/connect.php");
+
+    dbconn();
+
+    $REL_TPL->stdhead("Загрузка из Ventra");
+    $REL_TPL->stdfoot();
---
+++ www/upload_sap.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: Reaplay
+     * Date: 10.07.2016
+     * Time: 23:14
+     */
+    require_once ("include/connect.php");
+
+    dbconn();
+
+    $REL_TPL->stdhead("Загрузка из SAP");
+    $REL_TPL->stdfoot();
